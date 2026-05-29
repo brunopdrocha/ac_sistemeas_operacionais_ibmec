d1ea0afd014e3cd63c09c9f2d3672672a37cb51c
brunopdrocha <brunopdrocha@gmail.com>
2026-05-29 18:56:26 -0300

    versao inicial

---
+++ main.sh
@@ -0,0 +1,7 @@
+!#/bin/bash
+# This script is used to run the application
+# It will start the application and keep it running until it is stopped
+# Start the application
+
+clear
+echo "Starting the application..."
